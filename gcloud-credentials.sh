06b3fe3009abda45b5c04c708490e82f3f3a5d9f
thuc.le1 <thuc.le1@mservice.com.vn>
2022-08-11 23:56:37 +0700

    Update script final

--- gcloud-credentials.sh
+++ gcloud-credentials.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo "$1" > config.json
 
-ls -la
-
-cat ./config.json
-
 export GOOGLE_APPLICATION_CREDENTIALS="./config.json"
 
-echo "$(GOOGLE_APPLICATION_CREDENTIALS)"
+echo "$GOOGLE_APPLICATION_CREDENTIALS"
 
-cat $(GOOGLE_APPLICATION_CREDENTIALS)
+cat $GOOGLE_APPLICATION_CREDENTIALS
