b13d52b705ea1da56e07190baf7d01a9b6fa9706
thuc.le1 <thuc.le1@mservice.com.vn>
2022-08-11 23:50:23 +0700

    Debug script

--- gcloud-credentials.sh
+++ gcloud-credentials.sh
@@ -2,6 +2,7 @@
 echo "$1" > config.json
 ls -la
 cat ./config.json
+echo "$(pwd)/config.json"
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/config.json"
 echo "$(GOOGLE_APPLICATION_CREDENTIALS)"
 cat $(GOOGLE_APPLICATION_CREDENTIALS)
